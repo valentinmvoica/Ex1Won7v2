c16dbd8487c6436bab675a11043871180401f078
v <v@DESKTOP-DR0IDJ0>
2024-02-16 23:17:58 +0200

    Am schimbat codul

--- Ex1Won7v2/Program.cs
+++ Ex1Won7v2/Program.cs
@@ -2,19 +2,13 @@
 Console.WriteLine("introduceti latime");
 int latime = int.Parse(Console.ReadLine());
 
-//Console.WriteLine("introduceti lungime");
-//int lungime= int.Parse(Console.ReadLine());
+Console.WriteLine("introduceti lungime");
+int lungime= int.Parse(Console.ReadLine());
 
 Console.WriteLine("introduceti intaltime");
 int inaltime= int.Parse(Console.ReadLine());
 
-if (latime == inaltime)
-{
-    Console.WriteLine("este patrat");
-}
-
-Console.WriteLine("Gata");
-//int volum = latime*lungime*inaltime;
-//Console.WriteLine("Volumul este " + volum);
+int volum = latime*lungime*inaltime;
+Console.WriteLine("Volumul este " + volum);
 
         
