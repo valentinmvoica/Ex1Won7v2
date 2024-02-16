eea8109a170b61253d7b76ecdd28834bac207afe
v <v@DESKTOP-DR0IDJ0>
2024-02-16 23:15:51 +0200

    Add project files.

---
+++ Ex1Won7v2/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("introduceti latime");
+int latime = int.Parse(Console.ReadLine());
+
+//Console.WriteLine("introduceti lungime");
+//int lungime= int.Parse(Console.ReadLine());
+
+Console.WriteLine("introduceti intaltime");
+int inaltime= int.Parse(Console.ReadLine());
+
+if (latime == inaltime)
+{
+    Console.WriteLine("este patrat");
+}
+
+Console.WriteLine("Gata");
+//int volum = latime*lungime*inaltime;
+//Console.WriteLine("Volumul este " + volum);
+
+        
